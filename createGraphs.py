33b31ff5dc23e529f1546f7309ca07cb2a8ade60
Evan Douglas Foglio <evan.foglio@maine.edu>
2021-11-04 15:36:42 -0400

    Now creates a png file for each set of data from sql

--- createGraphs.py
+++ createGraphs.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import time
 import sqlite3
 import numpy as np
@@ -50,4 +50,51 @@ for row in data:
 	Engine_oil_temperature.append(row[8])
 	datetime.append(row[9])
 
+
+data = [Engine_coolant_temperature, 
+	Fuel_pressure, 
+	Engine_speed, 
+	Vehicle_speed, 
+	Intake_air_temperature, 
+	Throttle_position, 
+	Fuel_Tank_Level_Input, 
+	Fuel_Type, 
+	Engine_oil_temperature]
+
+graph_labelsY = ["Temperature", 
+		"Pressure", 
+		"Speed", 
+		"Speed", 
+		"Temperature", 
+		"Position", 
+		"Fuel_Tank_Level_Input", 
+		"Fuel_Type", 
+		"Temperature"]
+
+graph_titles = ["Engine_coolant_temperature", 
+		"Fuel_pressure", 
+		"Engine_speed", 
+		"Vehicle_speed", 
+		"Intake_air_temperature", 
+		"Throttle_position", 
+		"Fuel_Tank_Level_Input",
+		"Fuel_Type",
+		"Engine_oil_temperature"]
+
+export_names = [s + ".png" for s in graph_titles]
+
+for i in range(9):
+	temp_fig, ax = plt.subplots(1)
+	temp_fig.set_size_inches(8, 6)
+	plt.plot(datetime, data[i])
+	plt.xlabel("Time")
+	plt.ylabel(graph_labelsY[i])
+	plt.title(graph_titles[i])
+	plt.xticks(rotation = -45, ha="left", rotation_mode="anchor")
+	# Limit x-axis to 12 tick marks
+	ax.xaxis.set_major_locator(plt.MaxNLocator(12))
+	# Format to not cut off part of the dats on x-axis
+	plt.tight_layout()
+	#export to PNG for webserver displying
+	plt.savefig(export_names[i])
 
