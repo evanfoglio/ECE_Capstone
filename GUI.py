97de1aba79c27952c7c0083569d2347738994d2d
Evan Douglas Foglio <evan.foglio@maine.edu>
2021-12-05 13:57:25 -0500

    Fixed the formatting of the engine runtime and the trouble codes

--- GUI.py
+++ GUI.py
@@ -9,52 +9,47 @@ import time
 import sqlite3
 
 def updateEngineCoolantTemp():
-        img = ImageTk.PhotoImage( Image.open("Engine_coolant_temperature.png"))
-        labelEngineCoolantTemp = ttk.Label(tabFFD1, image = img,  text= "Test")
+        img = ImageTk.PhotoImage( Image.open("Engine Coolant Temperature.png"))
+        labelEngineCoolantTemp = ttk.Label(tabEngCoolant, image = img,  text= "Test")
         labelEngineCoolantTemp.image = img
         labelEngineCoolantTemp.grid(column=0, row=1)
 
 def updateEngineRPM():
-        img = ImageTk.PhotoImage( Image.open("Engine_RPM.png"))
-        labelEngSpeed = ttk.Label(tabFFD2, image = img,  text= "Test")
+        img = ImageTk.PhotoImage( Image.open("Engine RPM.png"))
+        labelEngSpeed = ttk.Label(tabEngRPM, image = img,  text= "Test")
         labelEngSpeed.image = img
         labelEngSpeed.grid(column=0, row=1)
 
 def updateVehicleSpeed():
-        img = ImageTk.PhotoImage( Image.open("Vehicle_speed.png"))
-        labelVehicleSpeed = ttk.Label(tabFFD3, image = img,  text= "Test")
+        img = ImageTk.PhotoImage( Image.open("Vehicle Speed.png"))
+        labelVehicleSpeed = ttk.Label(tabSpeed, image = img,  text= "Test")
         labelVehicleSpeed.image = img
         labelVehicleSpeed.grid(column=0, row=1)
 
 def updateIntakeAirTemperature():
-        img = ImageTk.PhotoImage( Image.open("Intake_air_temperature.png"))
-        labelIntakeAirTemperature = ttk.Label(tabFFD4, image = img,  text= "Test")
+        img = ImageTk.PhotoImage( Image.open("Intake Air Temperature.png"))
+        labelIntakeAirTemperature = ttk.Label(tabIntakeAirTemp, image = img,  text= "Test")
         labelIntakeAirTemperature.image = img
         labelIntakeAirTemperature.grid(column=0, row=1)
 
 def updateThrottlePosition():
-        img = ImageTk.PhotoImage( Image.open("Throttle_position.png"))
-        labelThrottlePosition = ttk.Label(tabFFD5, image = img,  text= "Test")
+        img = ImageTk.PhotoImage( Image.open("Throttle Position.png"))
+        labelThrottlePosition = ttk.Label(tabThrotPos, image = img,  text= "Test")
         labelThrottlePosition.image = img
         labelThrottlePosition.grid(column=0, row=1)
 
 def updateCalcEngineLoad():
-        img = ImageTk.PhotoImage( Image.open("Calc_engine_load.png"))
-        labelFuelTankLevelInput = ttk.Label(tabFFD6, image = img,  text= "Test")
-        labelFuelTankLevelInput.image = img
-        labelFuelTankLevelInput.grid(column=0, row=1)
+        img = ImageTk.PhotoImage( Image.open("Calculated Engine Load.png"))
+        labelCalcEngineLoad = ttk.Label(tabEngLoad, image = img,  text= "Test")
+        labelCalcEngineLoad.image = img
+        labelCalcEngineLoad.grid(column=0, row=1)
 
 def updateAbsoluteBarometricPressure():
-        img = ImageTk.PhotoImage( Image.open("Absolute_barometric_pressure.png"))
-        labelFuelType = ttk.Label(tabFFD7, image = img,  text= "Test")
+        img = ImageTk.PhotoImage( Image.open("Absolute Barometric Pressure.png"))
+        labelFuelType = ttk.Label(tabBaroPres, image = img,  text= "Test")
         labelFuelType.image = img
         labelFuelType.grid(column=0, row=1)
 
-def updateEngineOilTemperature():
-        img = ImageTk.PhotoImage( Image.open("Engine_oil_temperature.png"))
-        labelEngineOilTemperature = ttk.Label(tabFFD8, image = img,  text= "Test")
-        labelEngineOilTemperature.image = img
-        labelEngineOilTemperature.grid(column=0, row=1)
 
 def get_last(db, data):
 
@@ -76,7 +71,8 @@ def get_last(db, data):
 
 def runDTC():
 	os.system('./DTCParse.py')
-	DTC_code = get_last("DTC.db", "DTC")	
+	DTC_code = get_last("DTC.db", "DTC")
+	DTC_code = str(DTC_code[0][0])	
 	global lblDTC
 	lblDTC.destroy() 
 	lblDTC = ttk.Label(tabDTC, text=DTC_code)	
@@ -91,7 +87,9 @@ def updateRuntime():
 	runtime = get_last("engineRuntime.db", "Engine_runtime")
 	global lblRuntime
 	lblRuntime.destroy()
-	lblRuntime = ttk.Label(tabFFD9, text=runtime)
+	print(type(runtime[0][0]))
+	runtime = str(runtime[0][0]) + " Seconds" 
+	lblRuntime = ttk.Label(tabEngRuntime, text=runtime)
 	lblRuntime.grid(column=1, row=0)	
 
 #Create the Window
@@ -105,76 +103,70 @@ window.title("OBD-II Reader")
 #Tab Creation and Labeling
 tabs = ttk.Notebook(window)
 tabDTC = ttk.Frame(tabs)
-tabFFD = ttk.Frame(tabs)
-tabRTD = ttk.Frame(tabs)
-tabSET = ttk.Frame(tabs)
+tabCollectData = ttk.Frame(tabs)
+tabQuit = ttk.Frame(tabs)
 
-tabFFD1 = ttk.Frame(tabs)
-tabFFD2 = ttk.Frame(tabs)
-tabFFD3 = ttk.Frame(tabs)
-tabFFD4 = ttk.Frame(tabs)
-tabFFD5 = ttk.Frame(tabs)
-tabFFD6 = ttk.Frame(tabs)
-tabFFD7 = ttk.Frame(tabs)
-tabFFD8 = ttk.Frame(tabs)
-tabFFD9 = ttk.Frame(tabs)
+tabEngCoolant = ttk.Frame(tabs)
+tabEngRPM = ttk.Frame(tabs)
+tabSpeed = ttk.Frame(tabs)
+tabIntakeAirTemp = ttk.Frame(tabs)
+tabThrotPos = ttk.Frame(tabs)
+tabEngLoad = ttk.Frame(tabs)
+tabBaroPres = ttk.Frame(tabs)
+tabEngRuntime = ttk.Frame(tabs)
 
 #creates labels for tabs
 tabs.add(tabDTC, text="DTC")
-tabs.add(tabFFD, text="FFD")
-tabs.add(tabSET, text="Quit")
+tabs.add(tabCollectData, text="Collect Data")
+tabs.add(tabQuit, text="Quit")
 
-tabs.add(tabFFD1, text="FFD1")
-tabs.add(tabFFD2, text="FFD2")
-tabs.add(tabFFD3, text="FFD3")
-tabs.add(tabFFD4, text="FFD4")
-tabs.add(tabFFD5, text="FFD5")
-tabs.add(tabFFD6, text="FFD6")
-tabs.add(tabFFD7, text="FFD7")
-tabs.add(tabFFD8, text="FFD8")
-tabs.add(tabFFD9, text="FFD9")
+tabs.add(tabEngCoolant, text="Engine coolant Temp")
+tabs.add(tabEngRPM, text="Engine RPM")
+tabs.add(tabSpeed, text="Vehicle Speed")
+tabs.add(tabIntakeAirTemp, text="Intake Air Temp")
+tabs.add(tabThrotPos, text="Throttle Position")
+tabs.add(tabEngLoad, text="Engine Load")
+tabs.add(tabBaroPres, text="Absolute Barometric Pressure")
+tabs.add(tabEngRuntime, text="Engine Runtime")
 
-#create label on FFD tab
-lblFFD = ttk.Label(tabFFD, text= 'label2')
-lblFFD.grid(column=0, row=0)
+#create label on CollectData tab
+lblCollectData = ttk.Label(tabCollectData, text= '')
+lblCollectData.grid(column=0, row=0)
 
 #Buttons added to each tab to update the graph
-FFD1Update = Button(tabFFD1, text="Update Graph", command=updateEngineCoolantTemp)
-FFD1Update.grid(column=0, row = 0)
+EngCoolantUpdate = Button(tabEngCoolant, text="Update Graph", command=updateEngineCoolantTemp)
+EngCoolantUpdate.grid(column=0, row = 0)
 
-FFD2Update = Button(tabFFD2, text="Update Graph", command=updateEngineRPM)
-FFD2Update.grid(column=0, row = 0)
+EngRPMUpdate = Button(tabEngRPM, text="Update Graph", command=updateEngineRPM)
+EngRPMUpdate.grid(column=0, row = 0)
 
-FFD3Update = Button(tabFFD3, text="Update Graph", command=updateVehicleSpeed)
-FFD3Update.grid(column=0, row = 0)
+SpeedUpdate = Button(tabSpeed, text="Update Graph", command=updateVehicleSpeed)
+SpeedUpdate.grid(column=0, row = 0)
 
-FFD4Update = Button(tabFFD4, text="Update Graph", command=updateIntakeAirTemperature)
-FFD4Update.grid(column=0, row = 0)
+IntakeAirTempUpdate = Button(tabIntakeAirTemp, text="Update Graph", command=updateIntakeAirTemperature)
+IntakeAirTempUpdate.grid(column=0, row = 0)
 
-FFD5Update = Button(tabFFD5, text="Update Graph", command=updateThrottlePosition)
-FFD5Update.grid(column=0, row = 0)
+ThrotPosUpdate = Button(tabThrotPos, text="Update Graph", command=updateThrottlePosition)
+ThrotPosUpdate.grid(column=0, row = 0)
 
-FFD6Update = Button(tabFFD6, text="Update Graph", command=updateCalcEngineLoad)
-FFD6Update.grid(column=0, row = 0)
+EngLoadUpdate = Button(tabEngLoad, text="Update Graph", command=updateCalcEngineLoad)
+EngLoadUpdate.grid(column=0, row = 0)
 
-FFD7Update = Button(tabFFD7, text="Update Graph", command=updateAbsoluteBarometricPressure)
-FFD7Update.grid(column=0, row = 0)
+BaroPresUpdate = Button(tabBaroPres, text="Update Graph", command=updateAbsoluteBarometricPressure)
+BaroPresUpdate.grid(column=0, row = 0)
 
-FFD8Update = Button(tabFFD8, text="Update Graph", command=updateEngineOilTemperature)
-FFD8Update.grid(column=0, row = 0)
-
-FFD9Update = Button(tabFFD9, text="Update Value", command=updateRuntime)
-FFD9Update.grid(column=0, row = 1)
+EngRuntimeUpdate = Button(tabEngRuntime, text="Update Value", command=updateRuntime)
+EngRuntimeUpdate.grid(column=0, row = 1)
 
 
-#create label on SET tab
-#lblSET = ttk.Label(tabSET, text= 'label4')
-#lblSET.grid(column=0, row=0)
+#create label on Quit tab
+#lblQuit = ttk.Label(tabSET, text= 'label4')
+#lblQuit.grid(column=0, row=0)
 
 tabs.pack(expand=1, fill='both')
 
-#Button on the SET tab that closes the GUI when pressed
-quitButton = Button(tabSET, text="Quit", width=20, height=10,command=window.destroy)
+#Button on the Quit tab that closes the GUI when pressed
+quitButton = Button(tabQuit, text="Quit", width=20, height=10,command=window.destroy)
 quitButton.grid(column=5, row=5)
 
 
@@ -186,11 +178,11 @@ DTCButton.grid(column=0, row = 1)
 lblDTC = ttk.Label(tabDTC, text= 'Codes will appear here')
 lblDTC.grid(column=0, row=0)
 
-lblRuntime = ttk.Label(tabFFD9, text='Value will appear here')
+lblRuntime = ttk.Label(tabEngRuntime, text='Value will appear here')
 lblRuntime.grid(column=0, row=0)
 
-FFDCollect = Button(tabFFD, text="Collect New Data", command=runFFD)
-FFDCollect.grid(column=0, row = 0)
+CollectData = Button(tabCollectData, text="Collect New Data", command=runFFD)
+CollectData.grid(column=0, row = 0)
 
 
 window.mainloop()
