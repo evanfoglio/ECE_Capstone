ffb328ef1d3cf73cb71359518a31b724290017d6
Evan Douglas Foglio <evan.foglio@maine.edu>
2021-12-13 12:44:40 -0500

    Fixed get_VehicleSpeed return value.
    Removed depricated collectFFD function.
    
    Removed depricated global variables.
    
    Added function to export to SQL db and Eported all collected data to a db.

--- FFDParse.py
+++ FFDParse.py
@@ -6,31 +6,6 @@ import sqlite3
 from datetime import datetime
 import sys
 
-broker = "broker.mqtt-dashboard.com"
-port = 1883
-sendTopic = "OBDIISend2"
-receiveTopic = "OBDIIRec"
-randomVal = random.randint(0, 1000)
-client_id = "python-mqtt-%d" % randomVal
-username = "emqx"
-password = "public"
-vol_response = "init"
-
-client = m.connect_mqtt(client_id, broker, port, username, password)
-time.sleep(1)
-
-
-#message_flag = False
-#def on_message(client, userdata, msg):
-#	message = "%s" % msg.payload.decode()
-#	global vol_response
-#	global message_flag
-#	vol_response = message
-#	message_flag = True
-
-#m.subscribe(client, "OBDIIRec")
-#client.on_message = on_message
-
 def one_byte_response(cmd, client):
 
 	global message_flag
@@ -56,7 +31,14 @@ def one_byte_response(cmd, client):
 	message_flag = False
 		
 	response = (str(vol_response)).split()
-	print(response)
+	
+	if((len(response) < 4)):
+                #bad response
+                log("Invalid reply to 01 " + cmd + " in FFDParse.py: " + str(response))
+                print("Invalid reply to 01 " + cmd + " in FFDParse.py: " + str(response))
+                sys.exit(0)
+
+	
 	if response[2] != '41' and response[3] != cmd:
 		#bad response
 		log("Invalid reply to 01 " + cmd + " in FFDParse.py: " + str(response))
@@ -107,84 +89,67 @@ def log(text):
 
 def get_EngineCoolantTemp(client):
 	engine_coolant_temp = int(one_byte_response("05", client), 16) - 40
-	return datetime.now, engine_coolant_temp
+	sql_export("FFD.db", "EngCoolTemp", "temp", engine_coolant_temp)
+	return datetime.now(), engine_coolant_temp
 def get_EngineRPM(client):
         engine_rpm_bytes = two_byte_response("0C", client)
         engine_rpm_bytes[0] = int(engine_rpm_bytes[0], 16)
         engine_rpm_bytes[1] = int(engine_rpm_bytes[1], 16)
         engine_rpm = (256 * engine_rpm_bytes[0] + engine_rpm_bytes[1]) / 4
-        return datetime.now, engine_rpm
+        sql_export("FFD.db", "EngRPM", "rpm", engine_rpm)
+        return datetime.now(), engine_rpm
 
 def get_VehicleSpeed(client):
 	vehicle_speed = int(one_byte_response("0D", client), 16)
-	return datetime.now, vehicle_speed
+	sql_export("FFD.db", "VehicleSpeed", "speed", vehicle_speed)
+	return datetime.now(), vehicle_speed
 def get_IntakeAirTemp(client):
 	intake_air_temp = int(one_byte_response("0F", client), 16) - 40
-	return datetime.now, intake_air_temp
+	sql_export("FFD.db", "IntakeAriTemp", "temp", intake_air_temp)
+	return datetime.now(), intake_air_temp
 
 def get_ThrottlePos(client):
 	throttle_position = int(one_byte_response("11", client), 16) * (100.0/255.0)
-	return datetime.now, throttle_position
+	sql_export("FFD.db", "throttle_position", "pos", throttle_position)
+	return datetime.now(), throttle_position
 
 def get_EngineLoad(client):
 	calc_engine_load = int(one_byte_response("04", client), 16) * (100.0/255.0)
-	return datetime.now, calc_engine_load
+	sql_export("FFD.db", "EngLoad", "load", calc_engine_load)
+	return datetime.now(), calc_engine_load
 
 def get_AbsBarometricPressure(client):
-	absolute_barometric_pressure = int(one_byte_response("33", client))
-	return datetime.now, absolute_barometric_pressure
+	absolute_barometric_pressure = int(one_byte_response("33", client), 16)
+	sql_export("FFD.db", "AbsBaroPres", "pressure", absolute_barometric_pressure)
+	return datetime.now(), absolute_barometric_pressure
 
 def get_Runtime(client):
         engine_runtime_bytes = two_byte_response("1F", client)
         engine_runtime_bytes[0] = int(engine_runtime_bytes[0], 16)
         engine_runtime_bytes[1] = int(engine_runtime_bytes[1], 16)
         engine_runtime = (256 * engine_runtime_bytes[0]) + engine_runtime_bytes[1]
+        sql_export("FFD.db", "Runtime", "runtime", engine_runtime)
         return engine_runtime
 
-def collectFFD(client):	
 
-	#find eningine coolant temp (degree C)
-	engine_coolant_temp = get_EngineCoolantTemp(client)
 
-	#find Engine RPM	
-	engine_rpm = get_EngineRPM(client)
-
-	#find vehicle speed (km/h)
-	vehicle_speed = get_VehicleSpeed(client)
-	
-	#find intake air temp (degree C)
-	intake_air_temp = get_IntakeAirTemp(client)
-
-	#find throttle position (%)
-	throttle_position = get_ThrottlePos(client)
-	
-	#find the engine load (%)
-	calc_engine_load = get_EngineLoad(client)
-	
-	#find the absolute_barometric_pressure (kpa)
-	absolute_barometric_pressure = get_AbsBarometricPressure(client)
-	
-	#export into SQL
-	try:
-		#sqlcon = sqlite3.connect("school/capstone/FFD.db")
-		sqlcon = sqlite3.connect("FFD.db")
-		cursor = sqlcon.cursor()
+def sql_export(database, tablename, dataname, data):
+        #export into SQL
+        try:
+                sqlcon = sqlite3.connect(database)
+                cursor = sqlcon.cursor()
                 #insert collected data into the sqlite3 string
-		insertdata = "insert into data(Engine_coolant_temperature, Engine_RPM, Vehicle_speed , Intake_air_temperature, Throttle_position, Calc_engine_load, Absolute_barometric_pressure, datetime) values (\"{}\", \"{}\", \"{}\", \"{}\", \"{}\", \"{}\", \"{}\", \"{}\")".format(engine_coolant_temp, engine_rpm, vehicle_speed, intake_air_temp, throttle_position, calc_engine_load, absolute_barometric_pressure, datetime.now())
-
-		count = cursor.execute(insertdata)
-		sqlcon.commit()
-		cursor.close()
+                insertdata = "insert into {}({}, datetime) values (\"{}\", \"{}\")".format(tablename, dataname, data, datetime.now())
+                count = cursor.execute(insertdata)
+                sqlcon.commit()
+                cursor.close()
         #Error checking
-	except sqlite3.Error as error:
-		print("ERROR, ", error)
-	finally:
-		if sqlcon:
-			sqlcon.close()
+        except sqlite3.Error as error:
+                print("ERROR, ", error)
 
-	return engine_coolant_temp, engine_rpm, vehicle_speed, intake_air_temp, throttle_position, calc_engine_load, absolute_barometric_pressure
-
-
+        finally:
+                if sqlcon:
+                        sqlcon.close()
 
 
 
